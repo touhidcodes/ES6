55c7e0a2d5311e45de53f7ed288ba25ef751f347
touhidcodes <touhidcodes@gmail.com>
2022-10-27 22:41:20 +0600

    template.js

---
+++ template.js
@@ -0,0 +1,20 @@
+const firstName = "Abdul";
+const lastName = "karim";
+
+// Normal JS
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// ES6
+const fullName2 = `${firstName} ${lastName} is a good boy.`;
+console.log(fullName2);
+
+// Normal JS
+const multiLine = "Hay\n" + "How are you?\n" + "Are you missing me?";
+console.log(multiLine);
+
+// ES6
+const multiLine2 = `Hay
+How are you?
+Are you missing me?`;
+console.log(multiLine2);
