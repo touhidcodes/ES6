de0797602cac0fbe931e53c426e5949d22bd70bb
touhidcodes <touhidcodes@gmail.com>
2022-11-05 20:14:18 +0600

    arrow function

---
+++ arrow.js
@@ -0,0 +1,14 @@
+// Arrow Function
+const give5 = () => 5;
+console.log(give5());
+
+const add = (x, y) => x + y;
+console.log(add(5, 7));
+
+const multiLine = (num1, num2) => {
+  const sum = num1 + num2;
+  const diff = num1 + num2;
+  const total = sum * diff;
+  return total;
+};
+console.log(add(4, 7));
