5ada147c981b252314de14f0b600f3056312ed42
touhidcodes <touhidcodes@gmail.com>
2022-11-05 20:53:54 +0600

    Class

---
+++ class.js
@@ -0,0 +1,13 @@
+// Create a property function by Class
+class student {
+  constructor(sName, sID, sSchool) {
+    this.name = sName;
+    this.id = sID;
+    this.school = "BSMRSTU";
+  }
+}
+const touhid = new student("Touhid", 24);
+const toufik = new student("Toufik", 05);
+const jannatul = new student("Jannatul", 12);
+
+console.log(touhid, toufik, jannatul);
