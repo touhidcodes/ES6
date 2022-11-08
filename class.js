bf591642a0bad136c7fb5a7cc1965e0a3b5aa3e3
touhidcodes <touhidcodes@gmail.com>
2022-11-08 20:25:43 +0600

    x

--- class.js
+++ class.js
@@ -11,3 +11,11 @@ const toufik = new student("Toufik", 05);
 const jannatul = new student("Jannatul", 12);
 
 console.log(touhid, toufik, jannatul);
+
+class student {
+  constructor(sName, sID) {
+    this.name = sName;
+    this.id = sID;
+    this.school = "BSMRSTU";
+  }
+}
