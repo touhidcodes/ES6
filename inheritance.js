c28ae7bec1b98be020aa1f9807aa4dac9e73ac3b
touhidcodes <touhidcodes@gmail.com>
2022-11-10 20:33:58 +0600

    inheritance class

--- inheritance.js
+++ inheritance.js
@@ -1,10 +1,17 @@
 //Link 2 class
-class child {
+class fatherName {
+  constructor() {
+    this.fatherName = "AF Fathers";
+  }
+}
+class child extends fatherName {
   constructor(Cname, Cage) {
+    super();
     this.name = Cname;
     this.age = Cage;
   }
 }
 
 const child1 = new child("Arnold", 13);
-console.log(child1);
+const child2 = new child("Fedwrick", 09);
+console.log(child1, child2);
